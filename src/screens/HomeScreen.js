8b2862f6d4719cea2c08a8838bccab265de4be55
Bradley Serbus <brad@buddy.com>
2018-06-28 21:30:30 -0700

    Added easy grid.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { AsyncStorage, Button, Text, View } from 'react-native';
+import { Col, Row, Grid } from "react-native-easy-grid";
 
+const colStyle = {height: 50, justifyContent: 'center', alignItems: 'center'};
 const FBSDK = require('react-native-fbsdk');
 const {
   LoginManager,
@@ -13,9 +15,22 @@ export class HomeScreen extends React.Component {
   
     render() {
       return (
-        <View>
+        <View style={{ flex: 1, alignItems: 'center'}}>
           <Text>Call Me</Text>
           <Text>Grid Placeholder</Text>
+          
+            <Grid>
+              <Col style={{  backgroundColor: 'red', ...colStyle }}>
+                <Text>Name</Text>
+              </Col>
+              <Col style={{ backgroundColor: 'blue', ...colStyle }}>
+                <Text>Time Until Contact</Text>
+              </Col>
+              <Col style={{ backgroundColor: 'green', ...colStyle }}>
+                <Text>Edit</Text>
+              </Col>
+            </Grid>
+          
           <Button title="Add"></Button>
           <Button title="Delete"></Button>
           <Button title="Show me more of the app" onPress={this._showMoreApp} />
