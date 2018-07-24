1929a219aeb56012509f2498901bb9cf19556634
Casey Cooper <caseykcooper14@gmail.com>
2018-07-23 21:48:23 -0700

    Add First Cut

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,38 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { AsyncStorage, Button, Text, View, StyleSheet, TouchableOpacity,
           Alert, TextInput } from 'react-native';
 import { Table, Row, Rows, Cell, TableWrapper } from 'react-native-table-component';
 
-const colStyle = {height: 50, justifyContent: 'center', alignItems: 'center'};
 const FBSDK = require('react-native-fbsdk');
 const {
   LoginManager,
 } = FBSDK;
 
-let personList = [
-  ['Joe Cool', '10', 'Medium', 'Gear Pic'],
+
+let personListHeader = ['Name', 'Days Remaining', 'Frequency', 'Edit'];
+
+enum frequency {
+  twice_A_Week,
+  once_A_Week,
+  once_Every_Two_Weeks,
+  once_Every_Three_Weeks,
+  once_Every_Four_Weeks
+}
+
+
+interface Person {
+  name: string;
+  phoneNumber: string;
+  lastCall: Date;
+  frequency: frequency;
+}
+
+let people: Person[] = [];
+let addPersonHeader = ['Add Person'];
+let defaultData: Person[] = [
+  {name: "Fake nAME", phoneNumber: "FAKE NUMBER",
+  lastCall: new Date(), frequency: frequency.once_Every_Two_Weeks}
 ];
 
-let person = [];
-let defaultData = [
-  ['Name'],
-  ['Phone Number'],
-  ['Days Remaining'],
-  ['Frequency'],
-];
 
+interface Props {
 
-export class HomeScreen extends React.Component {
-  constructor(props) {
+}
+
+interface State {
+  personTableData: Person[];
+  addPerson: boolean;
+  deletePerson: boolean;
+  addPersonTableData: Person[];
+}
+
+export class HomeScreen extends Component<Props, State> {
+  constructor(props: Props) {
     super(props);
+    
     this.state = {
-      tableHead: ['Name', 'Days Remaining', 'Frequency', 'Edit'],
-      tableData: personList,
+      personTableData: people,
       addPerson: false,
-      changeData: false,
-      text: '',
-      tableHeadTwo: ['Add Person'],
-      tableDataTwo: defaultData,
+      deletePerson: false,
+      addPersonTableData: defaultData,
     }
   }
   
@@ -46,36 +68,57 @@ export class HomeScreen extends React.Component {
       const state = this.state;
       let addTable;
       let confirmButton;
-      
+      let deleteTable;
 
       if (this.state.addPerson) {
+        let displayAddPersonTableData = state.addPersonTableData.map(person => {
+          return [person.name, person.phoneNumber, person.lastCall.toString(), person.frequency];
+        });
+        
         addTable = 
-        <TouchableOpacity onPress={this._changeDataCheck}>
+        <TouchableOpacity onPress={this._alertIndex}>
         <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
-          <Row data={state.tableHeadTwo} style={addPersonStyles.head} textStyle={addPersonStyles.text}/>
-          <Rows data={state.tableDataTwo} textStyle={addPersonStyles.text} />
+          <Row data={addPersonHeader} style={addPersonStyles.head} textStyle={addPersonStyles.text}/>
+          <Rows data={displayAddPersonTableData} textStyle={addPersonStyles.text} />
         </Table>
         </TouchableOpacity>
 
         confirmButton = <Button title="Confirm" onPress={this._addPersonCheck}></Button>
       }
+      if (this.state.deletePerson) {
+        deleteTable;
+      }
+
+      let displayPersonTableData = state.personTableData.map(person => {
+        return [person.name, person.phoneNumber, person.lastCall.toString(), person.frequency];
+      });
+
+     // console.log(JSON.stringify(displayPersonTableData));
+
       return (
         <View style={styles.container}>
           <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
-            <Row data={state.tableHead} style={styles.head} textStyle={styles.text}/>
-           <Rows data={state.tableData} textStyle={styles.text}/>
+            <Row data={personListHeader} style={styles.head} textStyle={styles.text}/>
+            <Rows data={displayPersonTableData} textStyle={styles.text}/>
           </Table>
 
+
           <Button title="Add" onPress={this._addPersonCheck}></Button>
-          <Button title="Delete"></Button>
+          <Button title="Delete" onPress={this._deletePersonCheck}></Button>
           <Button title="Show me more of the app" onPress={this._showMoreApp} />
           <Button title="Actually, sign me out :)" onPress={this._signOutAsync} />
 
           {addTable}
           {confirmButton}
          
+          
         </View>
+        
       );
+    }
+
+    _alertIndex() {
+      Alert.alert(`Yay`);
     }
 
     _addPersonCheck = () => {
@@ -84,19 +127,28 @@ export class HomeScreen extends React.Component {
           addPerson: true,
         })
       } else {
-        for (let i = 0; i < this.state.tableDataTwo.length; i++) {
-          person.push(this.state.tableDataTwo[i]);
+        for (let i = 0; i < this.state.addPersonTableData.length; i++) {
+          people.push(this.state.addPersonTableData[i]);
         }
-        personList.push(person);
-        person = [];
         this.setState({
-          tableData: personList,
+          personTableData: people,
           addPerson: false,
         })
       }
     }
-
   
+    _deletePersonCheck = () => {
+      if (!this.state.deletePerson){
+        this.setState({
+          deletePerson: true,
+        })
+      } else {
+        this.setState({
+          deletePerson: false,
+        })
+      }
+    }
+
     _showMoreApp = () => {
       this.props.navigation.navigate('Details');
     };
@@ -124,13 +176,4 @@ export class HomeScreen extends React.Component {
     btn: { width: 58, height: 18, backgroundColor: '#78B7BB',  borderRadius: 2 },
     btnText: { textAlign: 'center', color: '#fff' }
   });
-
-  
-  
-  
-
- 
-
-  
-
   
