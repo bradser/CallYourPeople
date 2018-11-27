7390c7266ef671628244bfafa577d50caff36bb7
Bradley Serbus <brad@buddy.com>
2018-11-26 20:19:17 -0800

    Set up notification opened listener.

--- App.tsx
+++ App.tsx
@@ -5,6 +5,8 @@ import { HomeScreen } from './src/screens/HomeScreen';
 import { DetailsScreen } from './src/screens/DetailsScreen';
 import { AuthLoadingScreen } from './src/screens/AuthLoadingScreen';
 import { SignInScreen } from './src/screens/SignInScreen';
+import { Linking } from 'react-native';
+import { NotificationsAndroid } from 'react-native-notifications';
 
 
 const AppStack = createStackNavigator({ Home: HomeScreen, Details: DetailsScreen });
@@ -35,3 +37,8 @@ export default class App extends React.Component {
     return <RootStack />;
   }
 }
+
+
+NotificationsAndroid.setNotificationOpenedListener((notification) => {
+  Linking.openURL('tel://' + notification.data.extra)
+});
